ac00120b7d430c748c49072dbab132c02e43a0b0
DropName <55547028+DropName@users.noreply.github.com>
2023-09-03 01:51:10 +0300

    +fixes
    made everything starting from 0 instead of 1

--- ayoclass.py
+++ ayoclass.py
@@ -2,35 +2,36 @@ class Ayogame:
     def __init__(self):
         self.state = [4,4,4,4,4,4,
                       4,4,4,4,4,4]
-    def move(self, player, hole):
-        # number of hole starting with 1 to 6
-        # number of player from 1 to 2
+        self.score = [0,0]
+
+    def move(self, hole):
+        # number of hole starting with 0 to 5
     
-        hole = hole + 6*(player - 1)
-    
-        if hole > 12 or hole < 1 or player < 1 or player > 2:
-            return False
-    
-        stones = self.state[hole-1]
-        self.state[hole-1] = 0
+        stones = self.state[hole]
+        self.state[hole] = 0
         shift = 0
     
         for i in range(stones):
-            if (hole + shift + i) % 12 == (hole-1) % 12:
+            if (hole + 1 + shift + i) % 12 == (hole) % 12:
                 shift += 1
         
-            self.state[(hole + i + shift) % 12] += 1
+            self.state[(hole + 1 + i + shift) % 12] += 1
 
-        last_hole = (hole + stones -1 + shift) % 12
+        last_hole = (hole + stones  + shift) % 12
         return last_hole
     
     def round(self, player, hole):
         # move till the capture
-        last_hole = self.move(player, hole) + 1
-        while self.state[last_hole-1]  > 1:
-            last_hole = self.move(player, last_hole) + 1
+    
+        last_hole = self.move(hole)
+        while self.state[last_hole] > 1:
+            last_hole = self.move(last_hole)
+            
+        if last_hole < 6:
+            self.score[player-1] = self.state[11 - last_hole]
+            self.state[11 - last_hole] = 0
 
-        return last_hole - 1
+        return last_hole
 
 
 
